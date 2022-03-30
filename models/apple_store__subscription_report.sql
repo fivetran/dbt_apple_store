aceba0ba7bb634e19cfbf227f2b8652b68ad35f4
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-03-30 12:06:03 -0700

    subscription model

---
+++ models/apple_store__subscription_report.sql
@@ -0,0 +1,75 @@
+with subscription_summary as (
+
+    select *
+    from {{ ref('int_apple_store__sales_subscription_summary') }}
+)
+
+, subscription_events as (
+
+    select *
+    from {{ ref('int_apple_store__sales_subscription_event_summary') }}
+)
+
+, reporting_grain as (
+
+    select
+        cast(date_day as date) as date_day
+        , account_id
+        , account_name
+        , app_name
+        , app_id
+        , subscription_name
+        , country
+        , state 
+    from subscription_summary
+    union 
+    select
+        cast(date_day as date) as date_day
+        , account_id
+        , account_name
+        , app_name
+        , app_id
+        , subscription_name
+        , country
+        , state 
+    from subscription_events
+)
+
+, joined as (
+
+    select 
+        reporting_grain.date_day
+        , reporting_grain.account_id
+        , reporting_grain.account_name 
+        , reporting_grain.app_id
+        , reporting_grain.subscription_name 
+        , reporting_grain.country
+        , reporting_grain.state
+        {% for event_val in var('apple_store__subscription_events') %}
+        {% set event_column = 'event_' ~ event_val | replace(' ', '_') | trim | lower %}
+        , coalesce({{ 'subscription_events.' ~ event_column }}, 0)
+            as {{ event_column }} 
+        {% endfor %}
+        , subscription_summary.active_free_trial_introductory_offer_subscriptions
+        , subscription_summary.active_pay_as_you_go_introductory_offer_subscriptions
+        , subscription_summary.active_pay_up_front_introductory_offer_subscriptions
+        , subscription_summary.active_standard_price_subscriptions
+    from reporting_grain
+    left join subscription_summary
+        on reporting_grain.date_day = subscription_summary.date_day
+        and reporting_grain.account_id =  subscription_summary.account_id 
+        and reporting_grain.app_name = subscription_summary.app_name
+        and reporting_grain.subscription_name = subscription_summary.subscription_name
+        and reporting_grain.country = subscription_summary.country
+        and reporting_grain.state = subscription_summary.state
+    left join subscription_events
+        on reporting_grain.date_day = subscription_events.date_day
+        and reporting_grain.account_id =  subscription_events.account_id 
+        and reporting_grain.app_name = subscription_events.app_name
+        and reporting_grain.subscription_name = subscription_events.subscription_name
+        and reporting_grain.country = subscription_events.country
+        and reporting_grain.state = subscription_events.state
+)
+
+select *
+from joined
---
+++ models/intermediate/int_apple_store__sales_subscription_event_summary.sql
@@ -0,0 +1,75 @@
+with base as (
+
+    select *
+    from {{ var('sales_subscription_event_summary') }}
+)
+
+, app as (
+    
+    select *
+    from {{ var('app') }}
+)
+
+, sales_account as (
+    
+    select * 
+    from {{ var('sales_account') }}
+)
+
+, filtered as (
+
+    select *
+    from base 
+    where lower(event)
+        in (
+            {% for event_val in var('apple_store__subscription_events') %}
+                {% if loop.index0 != 0 %}
+                , '{{ var("apple_store__subscription_events")[loop.index0] | trim |lower }}'
+                {% else %}
+                '{{ var("apple_store__subscription_events")[loop.index0] | trim | lower }}'
+                {% endif %}
+            {% endfor %}   
+        )
+)
+
+, pivoted as (
+    
+    select
+        date_day
+        , account_id
+        , app_name
+        , subscription_name
+        , country
+        , state
+        {% for event_val in var('apple_store__subscription_events') %}
+        , sum(case when lower(event) = '{{ event_val | trim | lower }}' then quantity else 0 end) as {{ 'event_' ~ event_val | replace(' ', '_') | trim | lower }}
+        {% endfor %}
+    from filtered
+    {{ dbt_utils.group_by(6) }}
+)
+
+, joined as (
+
+    select 
+        pivoted.date_day
+        , pivoted.account_id
+        , sales_account.account_name
+        , app.app_id
+        , pivoted.app_name
+        , subscription_name
+        , country
+        , case
+            when state is null then 'Not Available' else state
+          end as state 
+        {% for event_val in var('apple_store__subscription_events') %}
+        , {{ 'event_' ~ event_val | replace(' ', '_') | trim | lower }}
+        {% endfor %}
+    from pivoted
+    left join app 
+        on pivoted.app_name = app.app_name
+    left join sales_account 
+        on pivoted.account_id = sales_account.account_name
+)
+
+select *
+from joined
