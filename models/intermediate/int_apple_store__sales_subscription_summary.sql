0b3b9d5b69c8b1492cf03f41f05f6ebf0d84bd14
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-03-30 12:06:03 -0700

    subscription model

---
+++ models/intermediate/int_apple_store__sales_subscription_summary.sql
@@ -0,0 +1,42 @@
+with base as (
+
+    select *
+    from {{ var('sales_subscription_summary') }}
+)
+
+, app as (
+    
+    select *
+    from {{ var('app') }}
+)
+
+, sales_account as (
+    
+    select * 
+    from {{ var('sales_account') }}
+)
+
+, joined as (
+
+    select 
+        base.date_day
+        , base.account_id
+        , sales_account.account_name
+        , app.app_id
+        , base.app_name
+        , subscription_name
+        , country
+        , case
+            when state is null or trim(state) = '' then 'Not Available' else state
+          end as state  
+        , active_free_trial_introductory_offer_subscriptions
+        , active_pay_as_you_go_introductory_offer_subscriptions
+        , active_pay_up_front_introductory_offer_subscriptions
+        , active_standard_price_subscriptions
+    from base
+    left join app on base.app_name = app.app_name
+    left join sales_account on base.account_id = sales_account.account_id
+)
+
+select *
+from joined
