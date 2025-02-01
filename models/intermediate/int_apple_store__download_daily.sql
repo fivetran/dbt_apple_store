f61eafefe77164a420836015f585ec5f7359f1c3
Renee Li <renee.li@fivetran.com>
2025-01-31 23:29:57 -0600

    new int models

---
+++ models/intermediate/int_apple_store__download_daily.sql
@@ -0,0 +1,33 @@
+with base as (
+
+    select *
+    from {{ var('app_store_download_detailed_daily') }}
+),
+
+aggregated as (
+
+    select
+        date_day,
+        app_id,
+        download_type,
+        app_version,
+        device,
+        platform_version,
+        source_type,
+        page_type,
+        pre_order,
+        territory,
+        counts,
+        source_info,
+        page_title,
+        source_relation, 
+        sum(case when lower(download_type) = 'first-time download' then counts else 0 end) as first_time_downloads,
+        sum(case when lower(download_type) = 'redownload' then counts else 0 end) as redownloads,
+        sum(counts) AS total_downloads
+    from base
+    {{ dbt_utils.group_by(14) }}
+
+)
+
+select * 
+from aggregated
