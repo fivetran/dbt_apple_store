7d58645bb14869b92f75f360f51d05c93760b7ab
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-09-18 10:59:25 -0500

    bug/date-spine-query

--- models/intermediate/int_apple_store__date_spine.sql
+++ models/intermediate/int_apple_store__date_spine.sql
@@ -1,32 +1,36 @@
-{{ config(materialized='table') }}
+-- depends_on: {{ ref('stg_apple_store__app_store_installation_and_deletion_daily') }}
+-- depends_on: {{ ref('stg_apple_store__app_store_discovery_and_engagement_daily') }}
+-- depends_on: {{ ref('stg_apple_store__app_store_download_daily') }}
+-- depends_on: {{ ref('stg_apple_store__app_crash_daily') }}
+-- depends_on: {{ ref('stg_apple_store__app_session_daily') }}
 
 with spine as (
 
     {% if execute and flags.WHICH in ('run', 'build') %}
 
-{% set first_date_query %}
+        {% set first_date_query %}
 
-    select min(date_day) as min_date_day
-    from (
-        select cast(date as date) as date_day from {{ source('apple_store', 'app_store_installation_and_deletion_standard_daily') }}
-        union all
-        select cast(date as date) as date_day from {{ source('apple_store', 'app_store_discovery_and_engagement_standard_daily') }}
-        union all
-        select cast(date as date) as date_day from {{ source('apple_store', 'app_store_download_standard_daily') }}
-        union all
-        select cast(date as date) as date_day from {{ source('apple_store', 'app_crash_daily') }}
-        union all
-        select cast(date as date) as date_day from {{ source('apple_store', 'app_session_standard_daily') }}
-    ) as all_dates
+            select min(date_day) as min_date_day
+            from (
+                select date_day from {{ ref('stg_apple_store__app_store_installation_and_deletion_daily') }}
+                union all
+                select date_day from {{ ref('stg_apple_store__app_store_discovery_and_engagement_daily') }}
+                union all
+                select date_day from {{ ref('stg_apple_store__app_store_download_daily') }}
+                union all
+                select date_day from {{ ref('stg_apple_store__app_crash_daily') }}
+                union all
+                select date_day from {{ ref('stg_apple_store__app_session_daily') }}
+            ) as all_dates
 
-{% endset %}
+        {% endset %}
 
-{%- set first_date = dbt_utils.get_single_value(first_date_query) %}
+        {%- set first_date = dbt_utils.get_single_value(first_date_query) %}
 
-{% else %}
-{%- set first_date = '2024-01-01' %}
+        {% else %}
+        {%- set first_date = '2024-01-01' %}
 
-{% endif %}
+    {% endif %}
 
 {{
     dbt_utils.date_spine(
